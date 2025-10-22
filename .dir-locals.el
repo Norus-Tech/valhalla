cfb4477c5e502cee8148e23221876a9a7d1c5fee
Meshach Mitchell <meshach.mitchell@gmail.com>
2025-10-21 19:52:39 -0500

    style: minor cleanup

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,3 @@
 ((nil . ((indent-tabs-mode . nil)
          (c-basic-offset . 2)
-         (fill-column . 100)))
- ;; .h files are c++ headers here
- (auto-mode-alist . (("\\..*h$" . c++-mode))))
+         (fill-column . 100))))
